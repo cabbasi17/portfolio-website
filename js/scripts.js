b81380a3937699c68b51a51ce362d049f7967753
cabbasi17 <cabbasi17@gmail.com>
2025-01-13 13:47:02 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
-console.log("Hello World")
+// Function to toggle the mobile menu
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    // Check if the class name is 'navtoggle'. If so, add the 'responsive' class.
+    if (x.className === 'navtoggle') {
+        x.className += ' responsive';
+    } else {
+        // Otherwise, remove the 'responsive' class.
+        x.className = 'navtoggle';
+    }
+}
